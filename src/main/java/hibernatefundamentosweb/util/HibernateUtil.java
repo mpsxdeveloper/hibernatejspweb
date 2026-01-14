c2bc8aa1d08db6736e327ab0d3b19fc5cd15249a
mpsxdeveloper <98715891+mpsxdeveloper@users.noreply.github.com>
2026-01-14 15:56:35 -0400

    Classes do pacote util para conexão e utilização do Hibernate

---
+++ src/main/java/hibernatefundamentosweb/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package hibernatefundamentosweb.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory buildSessionFactory() {
+        
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().
+                configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().
+                build();
+        SessionFactoryBuilder sessionFactoryBuilder = metadata.getSessionFactoryBuilder();
+        sessionFactory = sessionFactoryBuilder.build();
+        return sessionFactory;
+    
+    }
+
+    public static SessionFactory getSessionFactory() {
+        
+        if(sessionFactory == null) {
+            sessionFactory = buildSessionFactory();
+        }
+        return sessionFactory;
+    
+    }
+    
+    public static void close() {
+        sessionFactory.close();
+    }
+    
+}
